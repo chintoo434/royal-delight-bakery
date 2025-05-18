66ecb4cdb3efc471ea5b5e6c9f863f04c62220f4
queryBytes <chintookumar434@gmail.com>
2025-05-18 13:39:22 +0530

    size dropdown added

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    name: '[C01] Doll Cake',
-    desc: 'to customize call to 9760648714',
-    sizes: [
-      { label: '1 pound', price: 300 },
-      { label: '2 pounds', price: 550 }
-    ],
-    img: 'img/C01.jpg',
-    category: 'Cake'
-  },
-  {
-    name: 'Black Forest Cake',
-    desc: 'Classic black forest cake',
-    sizes: [
-      { label: '1 pound', price: 250 },
-      { label: '2 pounds', price: 470 }
-    ],
-    img: 'img/C03.jpg',
-    category: 'Cake'
-  },
-  {
-    name: 'Red Velvet Cupcake',
-    desc: 'Creamy red velvet cupcake',
-    price: 150,
-    img: 'img/C02.jpg',
-    category: 'Cake'
-  },
-  // Add other products here...
-];
 
 const productList = document.getElementById('product-list');
 const cartItems = document.getElementById('cart-items');
